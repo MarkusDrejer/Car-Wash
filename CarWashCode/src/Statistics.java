dc6d3cf4f3dc21f365d79c03bad811566f783fc2
Nick Hansen <nick4630@stud.kea.dk>
2018-10-23 14:04:42 +0200

    Statistics

--- CarWashCode/src/Statistics.java
+++ CarWashCode/src/Statistics.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+
 public abstract class Statistics {
 
+private int count;
+private int revenue = 0;
+private ArrayList<String> washTypes;
+
+public void addWash() {
+
+}
+
+public void addRevenue(int newRevenue) {
+
+}
+
+public void addWashType(String type) {
+
+}
+
+public String getStats() {
+    return "lort";
+}
+
+private String findPopular() {
+
+}
 
 }
