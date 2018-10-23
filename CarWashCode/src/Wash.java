846c7bf01c505dbc0dd33a62fe447a3ebbf69c00
Nick Hansen <nick4630@stud.kea.dk>
2018-10-23 13:48:47 +0200

    Wash.java

--- CarWashCode/src/Wash.java
+++ CarWashCode/src/Wash.java
@@ -1,2 +1,32 @@
 public class Wash {
+
+    private String type;
+    private String length;
+    private int price;
+
+    public Wash(String Type, String Length, int Price) {
+
+    }
+
+    public void checkDiscount() {
+
+    }
+
+    public String getInfo() {
+    return "hej";
+    }
+
+    public void addStats() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
 }
