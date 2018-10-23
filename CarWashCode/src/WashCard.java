979bde36d6db9a9452ddf5396072cb69509575fb
ThomasVindelev <thom891b@stud.kea.dk>
2018-10-23 14:01:17 +0200

    Made changes in Recharge

--- CarWashCode/src/WashCard.java
+++ CarWashCode/src/WashCard.java
@@ -1,6 +1,25 @@
 public class WashCard {
 
-    private int PIN;
-    private
+    private int pin;
+    private int id;
+    private String name;
+    private int balance = 0;
+
+    public WashCard(int pin, String name) {
+
+    }
+
+    public void updateBalance(int newAmount) {
+
+    }
+
+    public int getBalance() {
+    return balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 
 }
