a32e014a38bb756cdefe018f3f1c6a4d3d40f40f
Nick Hansen <nick4630@stud.kea.dk>
2018-10-23 14:20:23 +0200

    Statistics

--- CarWashCode/src/Wash.java
+++ CarWashCode/src/Wash.java
@@ -1,15 +1,32 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Wash {
 
     private String type;
     private String length;
     private int price;
+    private int day;
+    private int time;
+    Date now = new Date();
 
-    public Wash(String Type, String Length, int Price) {
+    public Wash(String type, String length, int price) {
+        this.type = type;
+        this.length = length;
+        this.price = price;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        day = calendar.get(Calendar.DAY_OF_WEEK);
+        time = calendar.get(Calendar.HOUR_OF_DAY);
 
     }
 
     public void checkDiscount() {
-
+        int diff;
+    if( day != 7 && day != 6 && time < 14) {
+        diff = price * 20 / 100;
+        price = price - diff;
+    }
     }
 
     public String getInfo() {
