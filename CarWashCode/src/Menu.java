3ad7d69c399849e4e4046da80b749b825b74f931
MarkusDrejer <mabd412b@stud.kea.dk>
2018-10-23 13:55:45 +0200

    Added Menu

--- CarWashCode/src/Menu.java
+++ CarWashCode/src/Menu.java
@@ -4,10 +4,10 @@ public class Menu {
 
     public Menu(Scanner console){
         System.out.println("Welcome to the Car Wash System");
+        System.out.println("1. Wash Car" + "\n" + "2. Balance" + "\n" + "3. Recharge" +
+                "\n" + "4. Statistics" + "\n" + "0. Exit");
         int choice = console.nextInt();
         do {
-            System.out.println("1. Wash Car" + "\n" + "2. Balance" + "\n" + "3. Recharge" +
-                    "\n" + "4. Statistics" + "\n" + "0. Exit");
             choice = console.nextInt();
 
             switch (choice){
