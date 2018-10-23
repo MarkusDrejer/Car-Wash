bea4a40ba5f6dd8c7d65962e57c5dd44650fc2a0
JoachimGoodman <joac1087@stud.kea.dk>
2018-10-23 13:47:59 +0200

    Test

--- CarWashCode/src/Receipt.java
+++ CarWashCode/src/Receipt.java
@@ -1,2 +1,17 @@
 public class Receipt {
+    private int orderID;
+    private int total;
+    private String costumerName;
+    private int washCardId;
+    private String washType;
+
+    public String printReceipt() {
+
+    }
+    public Receipt(int orderID, String costumerName, int washCard, String washType) {
+
+    }
+    public Receipt(int orderID, String costumerName, int washCard) {
+
+    }
 }
