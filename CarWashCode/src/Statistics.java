7e891bf459d49fa9e69b8ddb1950cd329ba65564
MarkusDrejer <mabd412b@stud.kea.dk>
2018-10-25 11:09:25 +0200

    Small Changes

--- CarWashCode/src/Statistics.java
+++ CarWashCode/src/Statistics.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
         int type3 = 0;
 
         for (int i = 0; i < washTypes.size(); i++) {
-            switch (washTypes) {
+            switch (washTypes.get(i)) {
                 case 1:
                     type1++;
                     break;
