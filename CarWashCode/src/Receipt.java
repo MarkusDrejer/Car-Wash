db4474eb2e050121cc78627de7ad816e5bf4f366
Nick Hansen <nick4630@stud.kea.dk>
2018-10-25 10:41:20 +0200

    Receipt made

--- CarWashCode/src/Receipt.java
+++ CarWashCode/src/Receipt.java
@@ -6,7 +6,8 @@ public class Receipt {
     private String washType;
 
     public String printReceipt() {
-        return "Your orderId: " + orderID + "\n";
+        return "Your order id: " + orderID + "\n" + "Your total: " + total + "\n" + "Your name: " + costumerName + "\n" +
+                "Your Washcard id: " + washCardId + "\n" + "Wash type: " + washType;
     }
     public Receipt(int orderID, String costumerName, int washCardId, String washType) {
         this.orderID = orderID;
