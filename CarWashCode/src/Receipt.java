1c7f43879571bb0711e69aba5a2ae0d6332557f6
MarkusDrejer <mabd412b@stud.kea.dk>
2018-10-25 10:49:07 +0200

    Updated Receipt

--- CarWashCode/src/Receipt.java
+++ CarWashCode/src/Receipt.java
@@ -5,11 +5,6 @@ public class Receipt {
     private int washCardId;
     private String washType;
 
-    public String printReceipt() {
-        return "Your order id: " + orderID + "\n" + "Your total: " + total + "\n" + "Your name: " + costumerName + "\n" +
-                "Your Washcard id: " + washCardId + "\n" + "Wash type: " + washType;
-
-    }
     public Receipt(int orderID, String costumerName, int washCardId, String washType) {
         this.orderID = orderID;
         this.costumerName = costumerName;
@@ -20,5 +15,10 @@ public class Receipt {
         this.orderID = orderID;
         this.costumerName = costumerName;
         this.washCardId = washCardId;
+    }
+    public String toString() {
+        return "Your order id: " + orderID + "\n" + "Your total: " + total + "\n" + "Your name: " + costumerName + "\n" +
+                "Your Washcard id: " + washCardId + "\n" + "Wash type: " + washType;
+
     }
 }
